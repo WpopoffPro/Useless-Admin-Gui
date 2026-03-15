0f2000eaccda463e39706ef7499f31994415b521
WpopoffPro <wpopoffpro@gmail.com>
2026-03-15 15:51:02 +0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,18 +1,1 @@
-local Rayfield
-local RAYFIELD_SUCCESS, RAYFIELD_FAIL = pcall(function()
-Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
-end)
-if not RAYFIELD_SUCCESS then
-local RAYFIELD_SUCCESS, RAYFIELD_FAIL = pcall(function()
-Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua"))()
-end)
-if not RAYFIELD_SUCCESS then
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "Failed to launch rayfield.",
-	Text = "Please check your internet connection, or try using VPN.",
-  Icon = "rbxassetid://125704683916878",
-	Duration = 10,
-})
-return
-end
-end
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
